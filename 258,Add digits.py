99a325d72e7673d9e4ae38a7a7d9d04e0ad1bac0
Himanshu_Gupta <62868816+himanshu2801@users.noreply.github.com>
2020-05-07 00:35:03 +0530

    Create 258,Add digits.py

---
+++ 258,Add digits.py
@@ -0,0 +1,19 @@
+"""
+Question...
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+Example:
+
+Input: 38
+Output: 2 
+Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2. 
+             Since 2 has only one digit, return it.
+            
+Solution...
+"""
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num%9==0 and num!=0:
+            return 9
+        else:
+            return num%9
