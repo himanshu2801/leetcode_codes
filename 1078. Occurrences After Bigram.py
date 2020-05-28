9e1cfdb672b772f290c6835305b291e2bc264b2f
Himanshu_Gupta <62868816+himanshu2801@users.noreply.github.com>
2020-05-28 18:29:00 +0530

    Create 1078. Occurrences After Bigram.py

---
+++ 1078. Occurrences After Bigram.py
@@ -0,0 +1,36 @@
+"""
+Question...
+Given words first and second, consider occurrences in some text of the form "first second third", where second comes immediately after first, and third comes immediately after second.
+
+For each such occurrence, add "third" to the answer, and return the answer.
+
+ 
+
+Example 1:
+
+Input: text = "alice is a good girl she is a good student", first = "a", second = "good"
+Output: ["girl","student"]
+Example 2:
+
+Input: text = "we will we will rock you", first = "we", second = "will"
+Output: ["we","rock"]
+ 
+
+Note:
+
+1 <= text.length <= 1000
+text consists of space separated words, where each word consists of lowercase English letters.
+1 <= first.length, second.length <= 10
+first and second consist of lowercase English letters.
+Solution...
+"""
+class Solution:
+    def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
+        lst=list(text.split(" "))
+        lst1=[]
+        for i in range(len(lst)-2):
+            if lst[i]==first and lst[i+1]==second:
+                lst1.append(lst[i+2])
+        return lst1
+                
+        
