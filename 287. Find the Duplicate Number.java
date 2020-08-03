0bebe493ecdb21e9f3919eba06f0c445fcaf7415
Himanshu_Gupta <62868816+himanshu2801@users.noreply.github.com>
2020-08-03 11:45:39 +0530

    Update and rename 287. Find the Duplicate Number.py to 287. Find the Duplicate Number.java

--- 287. Find the Duplicate Number.java
+++ 287. Find the Duplicate Number.java
@@ -12,9 +12,24 @@ Input: [3,1,3,4,2]
 Output: 3
 Solution...
 """
+class Solution {
+    public int findDuplicate(int[] nums) {
+        
+        for(int i=0;i<nums.length;i++){
+            for(int j=i;j<nums.length;j++){
+                if (nums[i]-nums[j]==0 && i!=j){
+                    return nums[i];
+                }
+            }
+        }
+        return 0;
+    }
+}
+"""
+Python version 
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         for i in nums:
             if nums.count(i)>1:
                 return i
-        
+        """
