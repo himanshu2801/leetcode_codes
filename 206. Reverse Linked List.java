ebc5664bab4851c8c782f831e74c1ab9314c16de
Himanshu_Gupta <62868816+himanshu2801@users.noreply.github.com>
2020-08-06 13:12:19 +0530

    Update 206. Reverse Linked List.java

--- 206. Reverse Linked List.java
+++ 206. Reverse Linked List.java
@@ -36,3 +36,31 @@ class Solution {
         return head;
     }
 }
+""" Iteration Method"""
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    static ListNode reverse(ListNode head){
+        ListNode prev=null;
+        while (head!=null){
+            ListNode temp=head.next;
+            head.next=prev;
+            prev=head;
+            head=temp;
+        }
+        return prev;
+        
+    }
+    public ListNode reverseList(ListNode head) {
+        head=reverse(head);
+        return head;
+    }
+}
