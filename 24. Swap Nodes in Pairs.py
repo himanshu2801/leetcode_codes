81cca767991928747ae1b0a1e61e4fe824469b90
Himanshu_Gupta <62868816+himanshu2801@users.noreply.github.com>
2020-11-26 14:40:21 +0530

    Create 24. Swap Nodes in Pairs.py

---
+++ 24. Swap Nodes in Pairs.py
@@ -0,0 +1,54 @@
+"""
+Given a linked list, swap every two adjacent nodes and return its head.
+
+You may not modify the values in the list's nodes. Only nodes itself may be changed.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+Example 2:
+
+Input: head = []
+Output: []
+Example 3:
+
+Input: head = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 100].
+0 <= Node.val <= 100
+"""
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        ListNode jump=dummy,first=head;
+        dummy.next=head;
+        while(first != null && first.next != null) {
+            ListNode second = first.next;
+            ListNode t=second.next;
+            second.next=first;
+            first.next=t;
+            jump.next=second;
+            jump=first;
+            first=t;
+        }
+        return dummy.next;
+    }
+}
